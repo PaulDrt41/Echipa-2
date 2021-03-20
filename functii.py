9b844b00f2044931d5684fc1fedef97c1d89d7c7
PaulDrt41 <paul.iusitn2005@gmail.com>
2021-03-20 17:32:00 +0200

    functii2.0 asd
    asdasd

---
+++ functii.py
@@ -0,0 +1,10 @@
+def attack():
+    rasp1 =int(input("You choose to attack, what will you use? \n-------- \n Made in Heaven(1)        Tusk Act 4 (2)"))
+    if (rasp == 1):
+        if (bhp > 100):
+            bhp = 1*bhp/2
+            eng-=20
+        else:
+            bhp = 1/3*bhp
+            eng-=15
+
